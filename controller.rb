75f98ae7b5a0ccf7426e2defa32aabc5ab7187e2
J Armstrong <jssc29@gmail.com>
2014-08-16 11:42:27 -0400

    Refactored controller.rb decks.rb view.rb

--- controller.rb
+++ controller.rb
@@ -2,7 +2,6 @@ require_relative "view.rb"
 require_relative "decks.rb"
 require 'pry'
 
-
 class Controller
   attr_reader :view
 
@@ -12,9 +11,9 @@ class Controller
 
   def run
     until false
-      view.clear_screen!
-      view.move_to_home!
+      view.reset_screen
       input = view.welcome_message
+
       case input
       when "songs"
         song_deck = Deck.new("songs")
@@ -22,50 +21,58 @@ class Controller
       when "movies"
         movie_deck = Deck.new("movies")
         play_deck(movie_deck)
-      when "exit"
-        return
-      when "i hate flashcards"
-        return
       else
-        puts "Please enter a valid category."
+        if exit?(input)
+          return
+        else
+          puts "Please enter a valid category."  # TODO put in view?
+          sleep 2
+        end
       end
     end
   end
 
   def correct_answer?(response, card)
     if response == card.answer
-      puts "Correct!!!".green
+      puts "Correct!!!".green                    # TODO put in view?
       1
     else
-      view.display_correct_answer
-      puts card.answer.green
+      view.display_correct_answer(card.answer)
       0
     end
   end
 
+  def exit?(answer)
+    return answer == "exit" || answer == "i hate flashcards" 
+  end
+
   def play_deck(deck)
-    view.clear_screen!
-    view.move_to_home!
+    view.reset_screen
     current_deck = deck.return_deck
     score = 0
 
     current_deck.each do |card|
-
+      # TODO play_card method?
       view.display_question(card)
       answer = gets.chomp.downcase
-      return if answer == "exit" || answer == "i hate flashcards"
-      score += correct_answer?(answer, card)
-      puts "\n"*2
+
+      if exit?(answer)
+        return 
+      else
+        score += correct_answer?(answer, card)
+        puts "\n"*2                             # TODO put in view?
+      end
     end
+
     view.score_msg(score)
-    puts "Thanks for playing!"
     sleep 3
   end
 
 end
 
-
-
-
 test = Controller.new
 test.run
+
+#===================== Tests ===================
+
+# TODO write tests
